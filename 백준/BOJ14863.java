c2366256536201aa6297594be7024795e74fd942
Sarang Park <jms20914@gmail.com>
2024-07-18 17:09:32 +0900

    [백준] 서울에서 경산까지

---
+++ 백준/BOJ14863.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ14863 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N, K;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        int[][] walk = new int[N + 1][2];
+        int[][] bike = new int[N + 1][2];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            walk[i][0] = Integer.parseInt(st.nextToken());
+            walk[i][1] = Integer.parseInt(st.nextToken());
+            bike[i][0] = Integer.parseInt(st.nextToken());
+            bike[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        int[][] dp = new int[N + 1][K + 1];
+        boolean[][] level = new boolean[N + 1][K + 1];
+        Arrays.fill(level[0], true);
+
+        for (int i = 1; i <= N; i++) {
+            // 도보
+            for (int j = 0; j <= K; j++) {
+                if (level[i - 1][j] && j + walk[i][0] <= K) {
+                    dp[i][j + walk[i][0]] = dp[i - 1][j] + walk[i][1];
+                    level[i][j + walk[i][0]] = true;
+                }
+            }
+            // 자전거
+            for (int j = 0; j <= K; j++) {
+                if (level[i - 1][j] && j + bike[i][0] <= K) {
+                    dp[i][j + bike[i][0]] = Math.max(dp[i][j + bike[i][0]], dp[i - 1][j] + bike[i][1]);
+                    level[i][j + bike[i][0]] = true;
+                }
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i <= K; i++) {
+            if (dp[N][i] > ans)
+                ans = dp[N][i];
+        }
+        System.out.println(ans);
+    }
+}
